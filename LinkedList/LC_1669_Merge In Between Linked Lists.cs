2f16b93690c6d68a649fed19c2e48a47ecf3dbb4
Vaibhav <vaibhavrevankar1995@gmail.com>
2021-01-26 21:54:52 +0530

    Added Solution for Leetcode 1669

---
+++ LinkedList/LC_1669_Merge In Between Linked Lists.cs
@@ -0,0 +1,37 @@
+﻿using DataStructures.Util;
+
+namespace DataStructures.LinkedList
+{
+    class LC_1669_Merge_In_Between_Linked_Lists
+    {
+        public ListNode MergeInBetween(ListNode list1, int a, int b, ListNode list2)
+        {
+            ListNode first = list1;
+            int i = 0;
+
+            while (i != a - 1)
+            {
+                first = first.next;
+                i++;
+            }
+
+            ListNode second = first;
+            while (i != b + 1)
+            {
+                second = second.next;
+                i++;
+            }
+
+            ListNode lastNode = list2;
+            while (lastNode.next != null)
+            {
+                lastNode = lastNode.next;
+            }
+
+            first.next = list2;
+            lastNode.next = second;
+            return list1;
+
+        }
+    }
+}
