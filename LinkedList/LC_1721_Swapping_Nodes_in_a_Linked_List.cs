34caa1edae251e280ce011fd1c5178c095b4b5a2
Vaibhav <vaibhavrevankar1995@gmail.com>
2021-01-26 21:57:16 +0530

    Added Solution for Leetcode 1721

---
+++ LinkedList/LC_1721_Swapping_Nodes_in_a_Linked_List.cs
@@ -0,0 +1,34 @@
+﻿using DataStructures.Util;
+
+namespace DataStructures.LinkedList
+{
+    class LC_1721_Swapping_Nodes_in_a_Linked_List
+    {
+        public ListNode SwapNodes(ListNode head, int k)
+        {
+            ListNode dummy = new ListNode(0, head);
+            ListNode dummy2 = dummy;
+            ListNode firstNode = null;
+            int i = 0;
+            while (dummy2 != null)
+            {
+                if (i < k)
+                {
+                    dummy2 = dummy2.next;
+                    firstNode = dummy2;
+                    i++;
+                }
+                else
+                {
+                    dummy = dummy.next;
+                    dummy2 = dummy2.next;
+                }
+            }
+
+            int temp = firstNode.val;
+            firstNode.val = dummy.val;
+            dummy.val = temp;
+            return head;
+        }
+    }
+}
