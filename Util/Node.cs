318fd29a3fba9c440b7e3c64bf604d92fe485689
Vaibhav <vaibhavrevankar1995@gmail.com>
2021-01-26 21:36:23 +0530

    Committing Util folder.

---
+++ Util/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Util
+{
+    public class Node
+    {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+    }
+}
